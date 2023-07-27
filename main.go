e93f3d81f634c5db81323b95adee5e8b0f9955fa
kris <lamia.mortis@icloud.com>
2023-07-27 23:41:01 +0200

    [00002-0] Adding ability to pass host via env variable, update response in case of error, set additional allowed origins

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
@@ -10,12 +11,18 @@ import (
 func main() {
     app := fiber.New()
 
+	host, found := os.LookupEnv("APP_HOST")
+
+    if !found {        
+		host = "ai-resolver.mind-ms"
+    }
+
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://ai-resolver.main",
+		AllowOrigins: "http://ai-resolver.main, http://ai-resolver-main, https://ai-resolver.main, https://ai-resolver-main",
 	}))
 	
     setUpApiRoutes(app)
-    app.Listen("ai-resolver.mind-ms:8888")
+    app.Listen(host + ":8888")
 }
 
 func setUpApiRoutes(app *fiber.App) {
